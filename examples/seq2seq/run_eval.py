8dff5016dd5938cb551ab4aae7cb35409c5d4c6f
TimHuang <timhuangxt@gmail.com>
2020-10-28 15:41:40 +0800

    for fine-tune results transfer

--- examples/seq2seq/run_eval.py
+++ examples/seq2seq/run_eval.py
@@ -99,7 +99,7 @@ def run_generate(verbose=True):
     parser.add_argument("--task", type=str, default="summarization", help="used for task_specific_params + metrics")
     parser.add_argument("--bs", type=int, default=8, required=False, help="batch size")
     parser.add_argument(
-        "--n_obs", type=int, default=-1, required=False, help="How many observations. Defaults to all."
+        "--n_obs", type=int, default=-1, required=False, help="How many observations (i.e., examples) used for evaluation. Defaults to all."
     )
     parser.add_argument("--fp16", action="store_true")
     parser.add_argument("--dump-args", action="store_true", help="print the custom hparams with the results")
@@ -115,12 +115,17 @@ def run_generate(verbose=True):
     parsed_args = parse_numeric_n_bool_cl_kwargs(rest)
     if parsed_args and verbose:
         print(f"parsed the following generate kwargs: {parsed_args}")
+
+    # to read all examples from val.source into this list 
     examples = [" " + x.rstrip() if "t5" in args.model_name else x.rstrip() for x in open(args.input_path).readlines()]
+    
     if args.n_obs > 0:
         examples = examples[: args.n_obs]
+
     Path(args.save_path).parent.mkdir(exist_ok=True)
     if args.reference_path is None and Path(args.score_path).exists():
         warnings.warn(f"score_path {args.score_path} will be overwritten unless you type ctrl-c.")
+        
     runtime_metrics = generate_summaries_or_translations(
         examples,
         args.save_path,
